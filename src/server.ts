8c509ba0eff05677d000d734e962f90c7e240e75
kissenger <ivyterrace@hotmail.co.uk>
2025-10-13 21:57:38 +0100

    server update

--- src/server.ts
+++ src/server.ts
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url)) {
+// if (isMainModule(import.meta.url)) {
   const port = 4005;
   app.listen(port, (error) => {
     if (error) {
@@ -60,7 +60,7 @@ if (isMainModule(import.meta.url)) {
 
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
-}
+// }
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
