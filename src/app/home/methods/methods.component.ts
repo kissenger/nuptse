d26e5c68dd372dd1b75eb7533aa663fac407794e
kissenger <ivyterrace@hotmail.co.uk>
2025-10-21 12:32:17 +0100

    grr

--- src/app/home/methods/methods.component.ts
+++ src/app/home/methods/methods.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Output} from '@angular/core';
 import { MethodDescriptor, MethodDescriptorsArray, MethodsArray } from '@shared/types';
-import { METHODS_DB } from "@shared/methods.lib";
+import { METHODS_DB } from "@shared/methods.lib"
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NavService } from '@shared/services/nav.service';
@@ -27,32 +27,32 @@ export class MethodsComponent {
     private _utility: Utility
   ) {}
 
-  public get methods(): MethodDescriptorsArray {
-    let stage = this._utility.stageFromMethodName(this.selectedMethods[0]?.name);
-    if (!!stage) return METHODS_DB.filter( (m: MethodDescriptor) => m.name.toLowerCase().indexOf(stage)>0);
-    return METHODS_DB;
-  }
+  // public get methods(): MethodDescriptorsArray {
+  //   let stage = this._utility.stageFromMethodName(this.selectedMethods[0]?.name);
+  //   if (!!stage) return METHODS_DB.filter( (m: MethodDescriptor) => m.name.toLowerCase().indexOf(stage)>0);
+  //   return METHODS_DB;
+  // }
 
-  public applyFilter() {
-    this.filteredMethods = this.methods
-      .filter( (m:MethodDescriptor) => m.name.toLowerCase().indexOf(this.searchString.toLowerCase())>=0)
-      .filter( (m:MethodDescriptor) => !this.selectedMethods.find( ({name}) => name === m.name));
-  } 
+  // public applyFilter() {
+  //   this.filteredMethods = this.methods
+  //     .filter( (m:MethodDescriptor) => m.name.toLowerCase().indexOf(this.searchString.toLowerCase())>=0)
+  //     .filter( (m:MethodDescriptor) => !this.selectedMethods.find( ({name}) => name === m.name));
+  // } 
 
-  onFocus() {
-    this.applyFilter();
-  }
+  // onFocus() {
+  //   this.applyFilter();
+  // }
 
-  onBlur() {
-    this.filteredMethods = [];
-  }
+  // onBlur() {
+  //   this.filteredMethods = [];
+  // }
 
-  updateSelectedMethods(m:MethodDescriptor,operation:'add'|'remove') {
-    if (operation === 'add') this.selectedMethods.push(m);
-    else this.selectedMethods = this.selectedMethods.filter(({name}) => name !== m.name)
-    this.searchString = '';
-    this.methodsArrayUpdated.emit(this.selectedMethods);
-  }
+  // updateSelectedMethods(m:MethodDescriptor,operation:'add'|'remove') {
+  //   if (operation === 'add') this.selectedMethods.push(m);
+  //   else this.selectedMethods = this.selectedMethods.filter(({name}) => name !== m.name)
+  //   this.searchString = '';
+  //   this.methodsArrayUpdated.emit(this.selectedMethods);
+  // }
 
 }
 
