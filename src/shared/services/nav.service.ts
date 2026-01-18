cc4c609e7eec4ada09c7414d07d1c105a54b6677
kissenger <ivyterrace@hotmail.co.uk>
2026-01-18 12:24:00 +0000

    scrolling error removed (scroll to be fixed)

--- src/shared/services/nav.service.ts
+++ src/shared/services/nav.service.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { ElementRef, Injectable, QueryList } from '@angular/core';
 
 @Injectable({
@@ -9,15 +10,22 @@ export class NavService {
   private _anchors?: QueryList<ElementRef>;
   private _activeAnchor: string = '';
 
+  constructor(
+    private _scroller: ViewportScroller
+  ) {
+
+  }
+
   setAnchors(anchors: QueryList<ElementRef>) {
     this._activeAnchor = anchors.get(0)?.nativeElement.id;
     this._anchors = anchors;
   }
   
   scrollTo(anchor: string) {
-    const target = <ElementRef>this._anchors?.find(a => a.nativeElement.id === anchor);
-    this._activeAnchor = target.nativeElement.id;
-    target?.nativeElement.scrollIntoView({ behavior: "smooth", inline: "center" });
+    // const target = <ElementRef>this._anchors?.find(a => a.nativeElement.id === anchor);
+    // this._activeAnchor = target.nativeElement.id;
+    this._scroller.scrollToAnchor(anchor)
+    // target?.nativeElement.scrollIntoView({ behavior: "smooth", inline: "center" });
   }
 
   get activeAnchor() {
